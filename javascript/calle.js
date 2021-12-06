d50c705880f392184b397927985f90f9d489e525
DianaSOLher <69129000+DianaSOLher@users.noreply.github.com>
2021-12-05 21:48:14 -0600

    Create calle.js

---
+++ javascript/calle.js
@@ -0,0 +1,13 @@
+function validarCalle() {
+    var calle = document.getElementById('calle');
+    var filtro = /^[0-9a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙñÑ\s]+$/;
+
+    if (!filtro.test(calle.value)) {
+        alert('Por favor, ingresa una direccion valida');
+        calle.focus;
+        return false;
+    }
+    else
+        alert('Direccion correcta');
+        return true;
+}
