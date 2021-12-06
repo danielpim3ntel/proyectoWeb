1644b793877e4278877f1dee280f292c77c1a40e
DianaSOLher <69129000+DianaSOLher@users.noreply.github.com>
2021-12-05 21:47:56 -0600

    Create colonia.js

---
+++ javascript/colonia.js
@@ -0,0 +1,13 @@
+function validarColonia() {
+    var colonia = document.getElementById('colonia');
+    var filtro = /^[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙñÑ\s]+$/;
+
+    if (!filtro.test(colonia.value)) {
+        alert('Por favor, ingresa una direccion valida');
+        colonia.focus;
+        return false;
+    }
+    else
+        alert('Direccion correcta');
+        return true;
+}
