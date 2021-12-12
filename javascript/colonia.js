ba06774856ffeff787ebc1ab2656dfb254771d85
DianaSOLher <69129000+DianaSOLher@users.noreply.github.com>
2021-12-11 21:44:45 -0600

    Update colonia.js

--- javascript/colonia.js
+++ javascript/colonia.js
@@ -1,20 +1,5 @@
 function validarColonia() {
     var colonia = document.getElementById('colonia');
-    var filtro = /^[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙñÑ\s]+$/;
-
-    if (!filtro.test(colonia.value)) {
-        alert('Por favor, ingresa una direccion valida');
-        colonia.focus;
-        return false;
-    }
-    else
-        alert('Direccion correcta');
-        return true;
-}
-
-
-function validarColonia() {
-    var colonia = document.getElementById('colonia');
     var filtro = /^[a-zA-Z\s]+$/;
 
     if (!filtro.test(colonia.value)) {
