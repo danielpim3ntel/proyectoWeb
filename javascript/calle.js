6748ffca7ee64ea173ff2c7f3cf100f02f890c86
DianaSOLher <69129000+DianaSOLher@users.noreply.github.com>
2021-12-11 21:45:56 -0600

    Update calle.js

--- javascript/calle.js
+++ javascript/calle.js
@@ -1,18 +1,5 @@
 function validarCalle() {
     var calle = document.getElementById('calle');
-    var filtro = /^[0-9a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙñÑ\s]+$/;
-
-    if (!filtro.test(calle.value)) {
-        alert('Por favor, ingresa una direccion valida');
-        calle.focus;
-        return false;
-    }
-    else
-        alert('Direccion correcta');
-        return true;
-}
-function validarCalle() {
-    var calle = document.getElementById('calle');
     var filtro = /^[0-9a-zA-Z\s]+$/;
 
     if (!filtro.test(calle.value)) {
