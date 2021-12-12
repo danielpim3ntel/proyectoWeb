4d0107c31b5427754e6fd51716f15f6d2416a602
Daniel Pimentel <89604589+DarksideXzz@users.noreply.github.com>
2021-12-11 22:15:10 -0600

    Add files via upload

---
+++ php/recibeFormulario.php
@@ -0,0 +1,23 @@
+<?php
+    $boleta = $_POST['boleta'];
+    $nombre = $_POST['nombre'];
+    $nombre = $_POST['nombre'];
+    $apePaterno = $_POST['apePaterno'];
+    $apeMaterno = $_POST['apeMaterno'];
+    $fechaNac = $_POST['fechaNac'];
+    $genero = $_POST['genero'];
+    
+    $curp = $_POST['curp'];
+    $calle = $_POST['calle'];
+    $colonia = $_POST['colonia'];
+    $codigop = $_POST['codigop'];
+    $tel = $_POST['tel'];
+    $correo = $_POST['correo'];
+
+    $escuelaProcedencia = $_POST['escuelaProcedencia'];
+    $entidadProcedencia = $_POST['entidadProcedencia'];
+    $promedio = $_POST['promedio'];
+    $opcion = $_POST['opcion'];
+
+    echo $boleta;
+?>
