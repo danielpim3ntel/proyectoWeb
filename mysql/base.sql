6b1df71156b03fb2da7192b775cbde2ebbdb800d
DianaSOLher <69129000+DianaSOLher@users.noreply.github.com>
2021-12-13 21:22:21 -0600

    Update base.sql

--- mysql/base.sql
+++ mysql/base.sql
@@ -18,12 +18,15 @@ USE `baseProyecto`;
 
 /*Table structure for table `alumno` */
 
-DROP TABLE IF EXISTS `alumno`;
-DROP TABLE IF EXISTS `horariosExamen`;
-DROP TABLE IF EXISTS `asignacionExamen`;
-DROP TABLE IF EXISTS `laboratorios`;
-DROP TABLE IF EXISTS `grupos`;
 
+
+
+
+DROP TABLE IF EXISTS `grupos`;
+DROP TABLE IF EXISTS `asignacionExamen`;
+DROP TABLE IF EXISTS `horariosExamen`;
+DROP TABLE IF EXISTS `alumno`;
+DROP TABLE IF EXISTS `laboratorios`;
 CREATE TABLE `alumno` (
   `boleta` varchar(10) NOT NULL,
   `nombre` varchar(64) NOT NULL,
@@ -84,33 +87,32 @@ CREATE TABLE `asignacionExamen` (
 
 /*Data for the table `laboratorios` */
 insert into `laboratorios`(`id_laboratorio`,`nombre_laboratorio`) 
-values ('1','Computacion 1');
+values ('Lab1','Computacion 1');
 insert into `laboratorios`(`id_laboratorio`,`nombre_laboratorio`) 
-values ('2','Computacion 2');
+values ('Lab2','Computacion 2');
 insert into `laboratorios`(`id_laboratorio`,`nombre_laboratorio`) 
-values ('3','Computacion 3');
+values ('Lab3','Computacion 3');
 insert into `laboratorios`(`id_laboratorio`,`nombre_laboratorio`) 
-values ('4','Computacion 4');
+values ('Lab4','Computacion 4');
 insert into `laboratorios`(`id_laboratorio`,`nombre_laboratorio`) 
-values ('5','Computacion 5');
+values ('Lab5','Computacion 5');
 insert into `laboratorios`(`id_laboratorio`,`nombre_laboratorio`) 
-values ('6','Computacion 6');
+values ('Lab6','Computacion 6');
 
 /*Data for the table `grupos` */
 insert into `grupos`(`id_grupo`,`semestre`) 
-values ('1','primero');
+values ('G1','2022/2');
 insert into `grupos`(`id_grupo`,`semestre`) 
-values ('2','segundo');
-insert into `grupos`(`id_grupo`,`semestre`) 
-values ('3','tercer');
+values ('G2','2023/1');
+
 
 /*Data for the table `horariosExamen` */
 insert into `horariosExamen`(`id_horario`,`id_grupo`,`id_laboratorio`,`hora`) 
-values ('1','primero');
+values ('E1','G1','Lab1','21-01-05 10:00:00 AM');
 insert into `horariosExamen`(`id_horario`,`id_grupo`,`id_laboratorio`,`hora`)
-values ('2','segundo');
+values ('E2','segundo');
 insert into `grupos`(`id_grupo`,`semestre`) 
-values ('3','tercer 3');
+values ('E3','tercer 3');
 
 /*Data for the table `alumno` */
 
@@ -121,3 +123,4 @@ insert  into `alumno`(`boleta`,`nombre`,`apeP`,`apeM`,`fechaNac`,`curp`,`correo`
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
