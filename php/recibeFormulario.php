91cd427b0c7c3480c21777c2bf043d8b59fc3184
DianaSOLher <69129000+DianaSOLher@users.noreply.github.com>
2021-12-14 23:43:34 -0600

    Update recibeFormulario.php

--- php/recibeFormulario.php
+++ php/recibeFormulario.php
@@ -1,8 +1,30 @@
 
 <?php
+
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $conexion = mysqli_connect("localhost", "root", "", "baseProyecto");
+
+//Recibir los datos del formulario
+$boleta = $_POST['boleta'];
+$nombre = $_POST['nombre'];
+$apePaterno = $_POST['apePaterno'];
+$apeMaterno = $_POST['apeMaterno'];
+$fechaNac = $_POST['fechaNac'];
+$genero = $_POST['genero'];
+$fechaNac = $_POST['fechaNac'];
+$curp = $_POST['curp'];
+$calle = $_POST['calle'];
+$colonia = $_POST['colonia'];
+$alcaldia = $_POST['alcaldia'];
+$codigop = $_POST['codigop'];
+$tel = $_POST['telefono'];
+$correo = $_POST['correo'];
+$escuelaProcedencia = $_POST['escuelaProcedencia'];
+$entidadProcedencia = $_POST['entidadProcedencia'];
+$promedio = $_POST['promedio'];
+$opcion = $_POST['opcion'];
+
  
 // Check connection
 if($conexion === false){
