bccc09e7460ead9176e4b97a853274b2ba99342d
DianaSOLher <69129000+DianaSOLher@users.noreply.github.com>
2021-12-20 18:43:13 -0600

    Update borrar.php

--- borrar.php
+++ borrar.php
@@ -1,6 +1,19 @@
 <?php 
         $conexion = mysqli_connect("localhost", "root", "", "baseProyecto");
-        $boleta = $_GET['boleta'];
-        $sql = "DELETE FROM alumno WHERE boleta = $boleta";
-        $result = mysqli_query($conexion,$sql);
+        $boleta = mysqli_real_escape_string($conexion, $_REQUEST['boleta']);
+        
+        $sql = "DELETE FROM asignacionExamen WHERE boleta = '$boleta'";
+        if(mysqli_query($conexion, $sql)){
+                echo "Borrado exitoso";
+        } else{
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conexion);
+        }
+        
+        $sql2 = "DELETE FROM alumno WHERE boleta = '$boleta'";
+        
+        if(mysqli_query($conexion, $sql2)){
+                echo "Borrado exitoso";
+        } else{
+                echo "ERROR: Could not able to execute $sql2. " . mysqli_error($conexion);
+        }
 ?>
