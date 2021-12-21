570146f71cd4e830e828be1cc1e88c12a7e06ae6
Isaac1011 <82616212+Isaac1011@users.noreply.github.com>
2021-12-21 00:35:10 -0600

    Update recibeFormulario.php

--- php/recibeFormulario.php
+++ php/recibeFormulario.php
@@ -54,9 +54,102 @@ $sql = "INSERT INTO alumno (boleta,nombre,apePaterno,apeMaterno,fechaNac,genero,
 VALUES ('$boleta', '$nombre', '$apePaterno', '$apeMaterno', '$fechaNac', '$genero', '$curp', '$calle', '$colonia', '$alcaldia', '$codigop', 
 '$telefono', '$correo', '$escuelaProcedencia', '$entidadProcedencia', '$promedio','$opcion')";
 if(mysqli_query($conexion, $sql)){
-    echo "Records added successfully.";
+   // echo "Records added successfully.";
+   ?>
+
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+    <html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Practica 2: Equipo 4 ">
+        <title>Registro</title>
+
+        <!--link para Bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="css/formulario.css">
+    </head>
+
+    <body>
+        <div class="container" style="background-color:blue;">
+            <div class="text-center" style="color: white;">
+                <h1>REGISTRO DE DATOS GENERALES</h1>
+                <h2>Alumnos de Nuevo Ingreso</h2>
+                <h2>(Enero 2022)</h2>
+                <h2>Registro exitoso de <?php echo $nombre." ".$apePaterno." ".$apeMaterno ?> </h2>
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <form action="correo.php" method="post">
+                <input type="hidden" name="boleta" value="<?php echo $boleta; ?>">
+                <input class="btn btn-primary position-center" type="submit" value="Enviar pdf a mi correo" name="confirmar" >
+            </form>
+        </div>
+
+
+        <div class="mb-3">
+                <input class="btn btn-primary position-center" type="submit" value="Volver al formulario" onclick="location.href = '../index.html';">
+        </div>
+
+    </body>
+
+    </html>
+
+<?php
+    /***********************************************************************************************************************************************/
+    
+
+
+
+
+
+    /*******************************************Error en el registro*****************************************************************************/ 
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conexion);
+    //echo "ERROR: Could not able to execute $sql. " . mysqli_error($conexion);
+    ?>
+
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+    <html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Practica 2: Equipo 4 ">
+        <title>Registro</title>
+
+        <!--link para Bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="css/formulario.css">
+    </head>
+
+    <body>
+        <div class="container" style="background-color:blue;">
+            <div class="text-center" style="color: white;">
+                <h1>REGISTRO DE DATOS GENERALES</h1>
+                <h2>Alumnos de Nuevo Ingreso</h2>
+                <h2>(Enero 2022)</h2>
+                <h2>ERROR EN EL REGISTRO</h2>
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <input class="btn btn-primary position-center" type="submit" value="Volver al formulario" onclick="location.href = '../index.html';">
+        </div>
+
+    </body>
+
+    </html>
+
+<?php
+    /***********************************************************************************************************************************************/
 }
  
 $datos = "SELECT * FROM alumno";
